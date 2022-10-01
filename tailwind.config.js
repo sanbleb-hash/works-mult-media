a055fd99667acaf44d5168c4897497cdde45489b
sanbleb_bash <sanbleb9@hotmail.com>
2022-10-01 15:24:31 +0200

    added a hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,7 @@ module.exports = {
 				photography:
 					"url('https://images.unsplash.com/photo-1471341971476-ae15ff5dd4ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80')",
 			},
+			hero: '/hero-img.jpg',
 		},
 	},
 	plugins: [require('@tailwindcss/forms')],
