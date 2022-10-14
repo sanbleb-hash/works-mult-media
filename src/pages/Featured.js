86a38ce49959df8590086f0432191f2d93a6ff66
sanbleb_bash <sanbleb9@hotmail.com>
2022-10-14 13:46:21 +0200

    re routing featured component to context

--- src/pages/Featured.js
+++ src/pages/Featured.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import FeaturedDetails from '../components/featuredDetails';
+import { articleContext } from '../utils/store';
 
 export const Featured = () => {
+	const [loading, setLoading] = useState(false);
+	const { dispatch } = useContext(articleContext);
+	const { name } = useParams();
+
+	useEffect(() => {
+		const fetchArticles = async () => {
+			try {
+				setLoading(true);
+				const { data } = await axios.get(
+					`https://works-mult-media.onrender.com/api/articles?type=${name}`
+				);
+
+				dispatch({ type: 'ARTICLES_PAYLOAD', payload: data });
+				console.log(data);
+				setLoading(false);
+			} catch (err) {
+				toast.error(err);
+			}
+		};
+		fetchArticles();
+	}, [name, dispatch]);
+
+	if (loading) <h3 className='text-center text-orange-500'>loading...</h3>;
+
 	return (
 		<section className=' w-screen pt-[10vh] text-white min-h-[100vh] pb-[250px]  bg-black text-center '>
 			<FeaturedDetails />
